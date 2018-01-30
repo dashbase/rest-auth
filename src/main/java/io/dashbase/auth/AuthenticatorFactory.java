7f1957007fac289054887455abb29a6905ab8ed6
John Wang <john@dashbase.io>
2018-01-30 13:47:16 -0800

    implement discoverable

--- src/main/java/io/dashbase/auth/AuthenticatorFactory.java
+++ src/main/java/io/dashbase/auth/AuthenticatorFactory.java
@@ -4,8 +4,9 @@ package io.dashbase.auth;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.basic.BasicCredentials;
+import io.dropwizard.jackson.Discoverable;
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", defaultImpl = ConfigAuthenticatorFactory.class)
-public interface AuthenticatorFactory {
+public interface AuthenticatorFactory extends Discoverable {
     public Authenticator<BasicCredentials, AuthenticatedUser> authenticator();
 }
