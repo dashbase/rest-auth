849124e76625ebb3eec86d32b4a0448a5055edce
John Wang <john@dashbase.io>
2018-02-01 10:49:48 -0800

    clean up

--- src/main/java/io/dashbase/auth/client/RestAuthClient.java
+++ src/main/java/io/dashbase/auth/client/RestAuthClient.java
@@ -17,17 +17,21 @@ public class RestAuthClient {
     public static void main(String[] args) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         RestAuthenticatorFactory restAuthenticatorFactory = new RestAuthenticatorFactory();
-        restAuthenticatorFactory.setUrl("http://localhost:8080/v1/auth/validate");
+        restAuthenticatorFactory.setUrl("https://dashbase.io/login/basic");
 
         Authenticator<BasicCredentials, AuthenticatedUser> authenticator = restAuthenticatorFactory.authenticator();
 
-        BasicCredentials credentials = new BasicCredentials("admin", "blah");
+        BasicCredentials credentials = new BasicCredentials("admin", "admin");
 
         Optional<AuthenticatedUser> user = authenticator.authenticate(credentials);
 
         if (user.isPresent()) {
             UserProfile profile = user.get().getProfile();
-            System.out.println(mapper.writeValueAsString(profile));
+            if (profile != null) {
+                System.out.println(mapper.writeValueAsString(profile));
+            } else {
+                System.out.println("user not authenticated");
+            }
         } else {
             System.out.println("user not found");
         }
