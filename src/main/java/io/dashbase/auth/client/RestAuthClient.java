78c8059a09e0c1581720c2dfd5268873fb8abb51
John Wang <john@dashbase.io>
2018-02-01 10:56:42 -0800

    clean up and testing

--- src/main/java/io/dashbase/auth/client/RestAuthClient.java
+++ src/main/java/io/dashbase/auth/client/RestAuthClient.java
@@ -1,15 +1,11 @@
 package io.dashbase.auth.client;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dashbase.auth.AuthenticatedUser;
 import io.dashbase.auth.RestAuthenticatorFactory;
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.basic.BasicCredentials;
-import org.pac4j.core.credentials.UsernamePasswordCredentials;
 import org.pac4j.core.profile.UserProfile;
-import org.pac4j.http.credentials.authenticator.RestAuthenticator;
-import org.pac4j.http.profile.RestProfile;
 
 import java.util.Optional;
 
@@ -17,7 +13,7 @@ public class RestAuthClient {
     public static void main(String[] args) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         RestAuthenticatorFactory restAuthenticatorFactory = new RestAuthenticatorFactory();
-        restAuthenticatorFactory.setUrl("https://dashbase.io/login/basic");
+        restAuthenticatorFactory.setUrl("http://localhost:8080/v1/auth/validate");
 
         Authenticator<BasicCredentials, AuthenticatedUser> authenticator = restAuthenticatorFactory.authenticator();
 
