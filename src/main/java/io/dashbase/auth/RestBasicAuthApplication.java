c2f8319d90fdbf8fb0b57f539392a11723387f8f
Keita Fujii <keita@dashbase.io>
2018-05-15 12:53:02 -0700

    embed config into docker image

--- src/main/java/io/dashbase/auth/RestBasicAuthApplication.java
+++ src/main/java/io/dashbase/auth/RestBasicAuthApplication.java
@@ -6,9 +6,22 @@ import io.dropwizard.auth.AuthValueFactoryProvider;
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.basic.BasicCredentialAuthFilter;
 import io.dropwizard.auth.basic.BasicCredentials;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class RestBasicAuthApplication extends Application<RestBasicAuthConfiguration> {
+    @Override
+    public void initialize(Bootstrap<RestBasicAuthConfiguration> bootstrap) {
+        // Enable variable substitution with environment variables
+        bootstrap.setConfigurationSourceProvider(
+            new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                new EnvironmentVariableSubstitutor(false)
+            )
+        );
+    }
+
     @Override
     public void run(RestBasicAuthConfiguration conf, Environment env) throws Exception {
         Authenticator<BasicCredentials, AuthenticatedUser> authenticator = conf.auth.authenticator();
