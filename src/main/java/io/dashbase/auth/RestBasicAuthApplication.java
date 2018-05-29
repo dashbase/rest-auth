9d754398736f6e7b5e7e6a65846d17b825007420
Keita Fujii <keita@dashbase.io>
2018-05-29 15:40:12 -0700

    switch to dropwizard-template-config

--- src/main/java/io/dashbase/auth/RestBasicAuthApplication.java
+++ src/main/java/io/dashbase/auth/RestBasicAuthApplication.java
@@ -1,25 +1,19 @@
 package io.dashbase.auth;
 
+import de.thomaskrille.dropwizard_template_config.TemplateConfigBundle;
 import io.dropwizard.Application;
 import io.dropwizard.auth.AuthDynamicFeature;
 import io.dropwizard.auth.AuthValueFactoryProvider;
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.basic.BasicCredentialAuthFilter;
 import io.dropwizard.auth.basic.BasicCredentials;
-import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
-import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class RestBasicAuthApplication extends Application<RestBasicAuthConfiguration> {
     @Override
     public void initialize(Bootstrap<RestBasicAuthConfiguration> bootstrap) {
-        // Enable variable substitution with environment variables
-        bootstrap.setConfigurationSourceProvider(
-            new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
-                new EnvironmentVariableSubstitutor(false)
-            )
-        );
+        bootstrap.addBundle(new TemplateConfigBundle());
     }
 
     @Override
